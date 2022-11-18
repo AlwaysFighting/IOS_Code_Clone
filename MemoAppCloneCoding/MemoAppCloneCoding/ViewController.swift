73c121ed8504ebf7a5777c4251922a104809932e
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2022-11-19 03:03:18 +0900

    MemoApp 추가

---
+++ MemoAppCloneCoding/MemoAppCloneCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoAppCloneCoding
+//
+//  Created by 목정아 on 2022/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
